3524a2ddd890ab0fbe7b9498bab137f01585ec11
Brenden Stark <starkbrenden1@gmail.com>
2023-03-10 16:35:29 -0700

    added filtering

--- feed.js
+++ feed.js
@@ -1,11 +1,14 @@
-function populateContent() {
-    const feed = document.getElementById("feed");
-    var request = new XMLHttpRequest();
-    request.open("GET", "/feed.json", false);
-    request.send(null)
-    const contentObj = JSON.parse(request.responseText);
+var request = new XMLHttpRequest();
+request.open("GET", "/feed.json", false);
+request.send(null)
+const allContent = JSON.parse(request.responseText);
 
+let feed;
+let filterList;
+let filterSet = new Set();
 
+function populateContent(contentObj) {
+    feed.innerHTML = "";
     //example
     // {
     //     img: "...",
@@ -14,7 +17,12 @@ function populateContent() {
     //     keywords: ["..."],
     // }
 
-    contentObj.forEach(content => {
+    // Shuffles array for fun with current fixed input
+    const shuffledArray = contentObj.sort((a, b) => 0.5 - Math.random());
+
+    // populates vid objects
+    shuffledArray.forEach(content => {
+        // Populate feed with content
         newVid = document.createElement("a");
         newVid.classList.add("vid")
 
@@ -28,7 +36,7 @@ function populateContent() {
 
         thumbnail = document.createElement("img");
         thumbnail.classList.add("vidThumb");
-        thumbnail.src = "thumbnail/"+content.img;
+        thumbnail.src = "thumbnail/" + content.img;
 
         keywords = document.createElement("p");
         keywords.classList.add("vidKeywords");
@@ -44,4 +52,33 @@ function populateContent() {
     });
 }
 
-window.onload = (event) => {populateContent();}
+function fillFilter() {
+    // Populate filter pulldown
+    allContent.forEach(content => content.keywords.forEach(filterSet.add, filterSet));
+
+    filterSet.forEach(keyword => {
+        item = document.createElement("li");
+        item.textContent = keyword;
+        item.setAttribute("onclick", `filterContent("${keyword}")`);
+        filterList.appendChild(item);
+    });
+}
+
+// Creates pulldown to select filter
+function gamesPulldown() {
+
+}
+
+// Input of game name/keyword, only displays those keywords on content page
+function filterContent(keyword) {
+    console.log(keyword);
+    populateContent(allContent.filter(element => element.keywords.includes(keyword)))
+}
+
+window.onload = (event) => {
+    feed = document.getElementById("feed");
+    filterList = document.getElementById("filter");
+
+    populateContent(allContent);
+    fillFilter();
+}
