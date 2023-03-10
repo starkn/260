51a12618e5c431e1d82252249f508219a6c809ff
starkn <nathen.d.stark124@gmail.com>
2023-03-10 15:42:12 -0700

    Dynamic Loading Content Home Page

--- feed.js
+++ feed.js
@@ -1,40 +1,47 @@
-const feed = document.querySelector("#feed");
-const contentObj = JSON.parse();
+function populateContent() {
+    const feed = document.getElementById("feed");
+    var request = new XMLHttpRequest();
+    request.open("GET", "/feed.json", false);
+    request.send(null)
+    const contentObj = JSON.parse(request.responseText);
 
-//example
-// {
-//     img: "...",
-//     title: "...",
-//     user: "...",
-//     keywords: ["..."],
-//     id: 0
-// }
 
-for (const content in contentObj) {
-    newVid = document.createElement("a");
-    newVid.classList.add("vid")
+    //example
+    // {
+    //     img: "...",
+    //     title: "...",
+    //     user: "...",
+    //     keywords: ["..."],
+    // }
 
-    title = document.createElement("p");
-    title.classList.add("vidTitle");
-    title.textContent = content.title;
+    contentObj.forEach(content => {
+        newVid = document.createElement("a");
+        newVid.classList.add("vid")
 
-    creator = document.createElement("p");
-    creator.classList.add("vidCreator");
-    creator.textContent = content.user;
+        title = document.createElement("p");
+        title.classList.add("vidTitle");
+        title.textContent = content.title;
 
-    thumbnail = document.createElement("img");
-    thumbnail.classList.add("vidThumb");
-    thumbnail.src = content.img;
+        creator = document.createElement("p");
+        creator.classList.add("vidCreator");
+        creator.textContent = content.user;
 
-    keywords = document.createElement("p");
-    keywords.classList.add("vidKeywords");
-    keywords.textContent = content.keywords.join(", ");
+        thumbnail = document.createElement("img");
+        thumbnail.classList.add("vidThumb");
+        thumbnail.src = "thumbnail/"+content.img;
 
-    newVid.appendChild(title);
-    newVid.appendChild(creator);
-    newVid.appendChild(thumbnail);
-    newVid.appendChild(keywords);
-    newVid.href = "viewer.html"
+        keywords = document.createElement("p");
+        keywords.classList.add("vidKeywords");
+        keywords.textContent = content.keywords.join(", ");
 
-    feed.appendChild(newVid);
+        newVid.appendChild(title);
+        newVid.appendChild(creator);
+        newVid.appendChild(thumbnail);
+        newVid.appendChild(keywords);
+        newVid.href = "viewer.html"
+
+        feed.appendChild(newVid);
+    });
 }
+
+window.onload = (event) => {populateContent();}
