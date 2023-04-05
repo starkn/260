a8a71ec07f17022a14e78f333b5f46752359bf3f
Brenden Stark <starkbrenden1@gmail.com>
2023-04-05 17:14:18 -0600

    dumb

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ apiRouter.post('/user/login', async (req, res) => {
 	}
 	res.status(401).send({ msg: '\u26A0 Invalid username or password \u26A0' });
 	//const user = await DB.createUser(req.body.username, req.body.password);
-	
+
 });
 
 
